f51437ad984c3f35a08cc4fcf5f3b3b20d80d703
Steven <curnow93@gmail.com>
2016-03-10 05:03:03 +0000

    changed html

---
+++ html/story.php
@@ -0,0 +1,11 @@
+<p>Ever since my teenage years I have been interested in computers, learning everything I could about them, starting off researching how I could make my own software, how I could do various cool things on computers, being around people that were great with security, joining various communities around the internet to improve my knowledge about them, I had met many people on the way but had other interests that would stop me from pursuing a career in web development, I had always known that I was great with computers but when young would be put off by the idea of being seen as a "nerd", after 4 years of college and unsuccesful applications to get a start as an electrical engineer...</p>
+
+<p>My life became depressing and I was starting to lose hope, becoming angered by the reality not meeting my expectations and being told that I would need "experience" as if I hadn't tried to find it, I knew I was not the average person and was capable of better things, being told "no" all the time made me want to go onto better things, I knew that I was better than what people saw of me, I was doubted at something I wasn't made for, but knew deep down my passion lied with computers rather than electrics, I slowly moved on from pursuing a career in Electrical Engineering and to start learning as a web developer.</p>
+
+<p>A year later I had discovered codeschool as I was missing parts previously learnt when young, at the time I would do it as a hobby, this time I would learn more than just a basics as I knew I would need to know more than what I did to become a professional web developer. My skills started to go from hobbyist to full time web developer sticking to it around 7 hours a day, but when you enjoy it, that's not so hard.</p>
+
+<p>My advice for anyone that wants to go into web development, is that you need to find a guide, join a forum, meet people around you interested in the same thing, and then improve alongside them, if they're better than you, you will start to become as good as them.</p>
+
+<blockquote>"Zen Proverb: "To follow the path, look to the master, follow the master, walk with the master, see through the master, become the master."</blockquote>
+
+
